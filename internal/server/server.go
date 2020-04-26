bf6d6407cabfe013daf2a68c0f7be78c77912a61
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-26 15:55:01 +0300

    Fix gitignore file, include server folder

---
+++ internal/server/server.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"economics/products/internal/config"
+	"economics/products/internal/model"
+)
+
+type Server struct {
+	productsRepo *model.ProductRepository
+}
+
+func NewServer(r *model.ProductRepository) *Server {
+	return &Server{r}
+}
+
+func (s Server) Run() {
+	http.HandleFunc("/products", s.productsHandler)
+	port := fmt.Sprint(":", config.GetPort())
+	http.ListenAndServe(port, nil)
+}
+
+func (s Server) productsHandler(w http.ResponseWriter, r *http.Request) {
+	products := s.productsRepo.GetAll()
+	js, err := json.Marshal(products)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+}
