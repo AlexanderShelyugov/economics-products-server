8240c20c69e49734117ae99bc27dfeb3939ecccd
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-30 12:38:05 +0300

    Setup basic SQL migrations

--- internal/model/db.go
+++ internal/model/db.go
@@ -2,13 +2,29 @@ package model
 
 import (
 	"fmt"
+	"log"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"economics/products/internal/config"
 )
 
+func runMigrations(config *config.DBConfig) {
+	url := fmt.Sprint("postgres://", config.User, ":", config.Password, "@", config.Host, ":", config.Port, "/", config.Database, "?sslmode=disable")
+	m, err := migrate.New("file://internal/model/migrations", url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func Init() (*gorm.DB, error) {
 	config := config.GetDBConfig()
+	runMigrations(&config)
 	url := fmt.Sprint(
 		"host=", config.Host,
 		" port =", config.Port,
